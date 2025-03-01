18d533e7ed8e637166ce2e587f8ce31173521a26
pavan khatale <pvnkhatale@gmail.com>
2025-03-02 00:36:23 +0530

    initial commit

---
+++ src/constants/config.ts
@@ -0,0 +1,90 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Pavan Khatale",
+    fullName: "Pavan Khatale",
+    email: "johndoe@mail.com",
+  },
+  hero: {
+    name: "Pavan Khatale",
+    p: ["Experienced Full Stack Developer with 4 years in React.js and Node.js. 🚀"],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `Experienced Full Stack Developer with 4 years in React.js and Node.js. Skilled in building scalable web apps, developing RESTful APIs, and optimizing performance. Proficient in Redux, Express.js, MongoDB, and cloud services. Passionate about clean code, problem-solving, and delivering high-quality solutions while collaborating with cross-functional teams. 🚀`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
